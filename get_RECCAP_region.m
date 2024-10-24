bc9bd106886b52412f4ae170f207845072a12de9
Jamie Wilson <jamie.wilson@liverpool.ac.uk>
2024-10-24 22:22:51 +0100

    fixed bug, added map

--- get_RECCAP_region.m
+++ get_RECCAP_region.m
@@ -5,37 +5,28 @@
 % RECCAP region based on the 1deg RECCAP grid
 %
 % Inputs: 
-%   - lat:      latitude (mx1 array)
-%   - lon:      longitude (mx1 array , lon values between 0 to 360 degE)
+%   - lat:      latitude (mx1 or mxn array)
+%   - lon:      longitude (mx1 mxn array)
 %   - varargin: pairs of string and integer array for combining regions
 %           
 % Outputs:
-%   - region:       region number for given lon/lat pair (mx1 array)
+%   - region:       region number for given lon/lat pair (mx1 or mxn array)
 %   - region_names: region long names (mx1 cell of strings)
 %   - coords:       nearest lon and lat values (struct)
 %
 % Notes:
-% To combine regions: add any number of string/array pairs containing name
+% 1) To combine regions: add any number of string/array pairs containing name
 % of new region and region numbers to combine, e.g., 
 % get_RECCAP_region(lat,lon,'region1',[1 2 3],'region2',[3 4 5]);
 %
-% Default Region Numbering:
-% 1) Arctic
-% 2) North Atlantic
-% 3) North Pacific
-% 4) Subtropical North Atlantic
-% 5) Subtropical North Pacific
-% 6) Subtropical Indian
-% 7) Equatorial Atlantic
-% 8) Equatorial Pacific
-% 9) Subtropical South Atlantic
-% 10) Subtropical South Pacific
-% 11) Subtropical Indian
-% 12) Subantarctic
-% 13) Antarctic
+% 2) call without any inputs to show a map of the RECCAP2 regions and region indices listed
+% e.g., get_RECCAP_region();
 %
 % authors: Jamie D. Wilson (jamie.wilson@liverpool.ac.uk); Erik Fields (fields@ucsb.edu)
 % created: 30th January 2024
+% updated:
+%          24th October 2024, JDW: updated with all reccap regions; mxn
+%           arrays; automatic [0 360]E conversion; map
 % -------------------------------------------------------------------------
 
 function [ region , region_names , coords] = get_RECCAP_region ( data_lat , data_lon , varargin )
@@ -49,13 +40,22 @@ function [ region , region_names , coords] = get_RECCAP_region ( data_lat , data
 % - region_indices : numerical index of regions correspondng to region_names
 load('regions/reccap_regions_master.mat')
 
+% display regions as map if no inputs given
+if nargin==0
+    plot_regions(lon,lat,reccap_master,region_names,region_indices);
+    names=strcat(region_indices',region_names')';
+    fprintf('%s \n','Region indices and names:')
+    fprintf('%s \n',names{:})
+    return
+end
+
 % vectorise RECCAP region
 MASK = reshape(reccap_master,[],1);
-[LON,LAT]=meshgrid(lon,lat);
+[LAT,LON]=meshgrid(lat,lon);
 LON=reshape(LON,[],1);
 LAT=reshape(LAT,[],1);
 
-% remove land (==0) so as to find closest wet grid points
+% remove land (==NaN) so as to find closest wet grid points
 ind=isnan(MASK);
 MASK(ind)=[];
 LON(ind)=[];
@@ -92,22 +92,6 @@ if any(any(data_lon<0))
     data_lon(lonshift_ind)=(data_lon(lonshift_ind))+360;
 end
 
-% region names
-% region_names={...
-%     'Arctic',...
-%     'North Atlantic',...
-%     'North Pacific',...
-%     'Subtropical North Atlantic',...
-%     'Subtropical North Pacific',...
-%     'Subtropical Indian',...
-%     'Equatorial Atlantic',...
-%     'Equatorial Pacific',...
-%     'Subtropical South Atlantic',...
-%     'Subtropical South Pacific',...
-%     'Subtropical Indian',...
-%     'Subantarctic',...
-%     'Antarctic'};
-
 % update regions if requested
 if ~isempty(varargin)
 
@@ -136,10 +120,10 @@ if ~isempty(varargin)
 
 end
 
-% nearest point search
+% the actual work - nearest point search
 k=dsearchn([LON LAT],[data_lon data_lat]);
 
-% regions for given lat/lon 
+% get the regions for given lat/lon 
 region=MASK(k);
 
 % reshape regions to match lon/lat input dimensions if needed
@@ -155,7 +139,7 @@ coords.lat = LAT(k);
 
 % shift from [0 360]E and reshape if needed
 if flag_lonshift
-    coords.lon(lonshift_ind) = -(coords.lon(lonshift_ind)-360);
+    coords.lon(lonshift_ind) = coords.lon(lonshift_ind)-360;
 end
 if flag_lonlat_vector
     tmp=nan(lonlat_dims);
@@ -186,4 +170,52 @@ function [ mask_tmp , names_tmp , merge_ind ] = update_regions (mask_tmp , names
         names_tmp{loop_ind(i)}=loop_name;
     end
 
+end
+
+% -------------------------------------------------------------------------
+% subfunction: plot_regions
+% - plots RECCAP2 regions
+% -------------------------------------------------------------------------
+function [ ] = plot_regions(lon,lat,reccap_master,region_names,region_indices)
+
+    plot_data=rot90(reccap_master);
+    [coastlines] = make_coastlines(plot_data,lat);
+    figure;
+    imAlpha=ones(size(plot_data));
+    imAlpha(isnan(plot_data))=0;
+    imagesc(lon,lat,plot_data,'AlphaData',imAlpha); hold on;
+    caxis([1 size(region_names,2)+1]);
+    c=colorbar;
+    colormap(parula(size(region_names,2)+1));
+    set(c,'YTick',[1.5:size(region_names,2)+1],'YTickLabel',strcat(region_indices',region_names'));
+    set(gca, 'color', [0.8 0.8 0.8]);
+    set(gca,'YTickLabel',flipud(get(gca,'YTickLabel')));
+    xlabel('Longutude (deg E)')
+    ylabel('Latitude (deg N)')
+    title('RECCAP2 Regions')
+    axis normal
+    pbaspect([2 1 1])
+    plot(coastlines(:,1:2)'-1.5,coastlines(:,3:4)'-1.5,'k')
+
+end
+
+% -------------------------------------------------------------------------
+% subfunction: make_coastlines
+% - calculate coast outline for plot
+% -------------------------------------------------------------------------
+function [coastlines] = make_coastlines(data,lat)
+   
+    grid_mask=zeros(size(data,1),size(data,2));
+    grid_mask(~isnan(data))=1;
+    grid_mask = [grid_mask(:,1) grid_mask grid_mask(:,end)];
+    grid_mask = [grid_mask(1,:) ;grid_mask; grid_mask(end,:)];
+
+    dx=diff(grid_mask,1,2); 
+    dy=diff(grid_mask,1,1); 
+    [jv iv]=find(dx~=0);
+    [jh ih]=find(dy~=0);
+    vertedge=[iv+1 iv+1 lat(jv) lat(jv+1)];
+    horzedge=[ih ih+1 lat(jh+1) lat(jh+1)];
+    coastlines=[vertedge;horzedge];
+    
 end
